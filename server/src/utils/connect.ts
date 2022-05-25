d1f64f5838ca3fa0df7bf31eb3aab70059017e5a
LaNeitD18 <phanhuytien14@gmail.com>
2022-05-25 14:33:28 +0700

    Heroku test commit

--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -10,7 +10,7 @@ const connect = async () => {
       // perform actions on the collection object
       client.close();
     });
-    console.log("DB connected");
+    console.log("Connect MongoDB successfully");
   } catch (error) {
     console.error("could not connect to DB", dbUri);
     process.exit(1);

0fd98e7ebc62d68a44a366981dae0099df9e6671
CuteTN <quantiennghia001@gmail.com>
2022-05-30 21:28:33 +0700

    Add trash classification route

--- server/src/controllers/trashClassification.controller.ts
+++ server/src/controllers/trashClassification.controller.ts
@@ -3,12 +3,13 @@ import HttpStatus from 'http-status-codes'
 import { predictTrashType as predictTrashTypeService } from '../services/classification.service'
 
 export const predictTrashType: RequestHandler = async function(req, res) {
-  const { url }: { url: string | null | undefined } = req.body; 
+  const { url }: { url: string | null | undefined } = (req.body ?? {}); 
+
   if (!url)
     return res.status(HttpStatus.BAD_REQUEST).json({ message: '"url" field is required.' });
 
   try {
-    const predictionResult = predictTrashTypeService(url);
+    const predictionResult = await predictTrashTypeService(url);
     return res.status(HttpStatus.OK).json({ predictionResult });
   }
   catch (err) {
